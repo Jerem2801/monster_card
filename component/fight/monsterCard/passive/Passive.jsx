b2ed7e28d6552f9caaa3e11514bce4d7bba53ee6
Jerem <lheureux.jeremy2801@gmail.com>
2025-07-25 11:49:59 +0200

    Correction du contenu de l'action ou passif

--- component/fight/monsterCard/passive/Passive.jsx
+++ component/fight/monsterCard/passive/Passive.jsx
@@ -1,7 +1,7 @@
 import { getActionContent } from '../actions/lib/actionUtils';
 
 export default function Passive({ passive, addMonsterCard, monsterName }) {
-    const content = getActionContent(passive, true, addMonsterCard, monsterName,false);
+    const content = getActionContent(passive, true, addMonsterCard, monsterName,null,false);
 
     return (
         <div className="w-[112%] -ml-[6%] rounded-sm shadow-sm mb-2 p-2 px-10 italic bg-gray-300 py-1 text-sm">
